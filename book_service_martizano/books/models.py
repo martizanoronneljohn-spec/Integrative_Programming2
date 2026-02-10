27aaf0673881ed9f213d0fb9d39b1f8630e7ace1
martizanoronneljohn-spec <martizanoronneljohn@gmail.com>
2026-02-10 23:38:49 +0800

    Add files via upload

---
+++ book_service_martizano/books/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Book(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
